192fd74d9f03731040bbf3d445bdde93235db739
santinic <hireclaudio@gmail.com>
2022-10-22 14:45:03 +0100

    v3.0.3

--- how2.rb
+++ how2.rb
@@ -2,11 +2,11 @@ class How2 < Formula
   desc "AI for your terminal"
   homepage "https://how2terminal.com"
   if Hardware::CPU.intel?
-    url "https://github.com/santinic/how2/releases/download/v3.0.2/how2-macos-x64.tar.gz"
-    sha256 "c5b18ff96f4bf9f82db0729555cd1ef33e2eb703ac60b474dba0e5832d76ef5c"
+    url "https://github.com/santinic/how2/releases/download/v3.0.3/how2-macos-x64.tar.gz"
+    sha256 "1c46d6ad92cf5114982a330d8c270d1459d0dae269ccb6e5830016598f23ac29"
   else
-    url "https://github.com/santinic/how2/releases/download/v3.0.2/how2-macos-arm64.tar.gz"
-    sha256 "c7f9fbe53a704fdfb20c5ac914deea7da849bd3022387f6a5881f43d099bf585"
+    url "https://github.com/santinic/how2/releases/download/v3.0.3/how2-macos-arm64.tar.gz"
+    sha256 "004cb1f85c6de89de2960960d6f8f6f025035da53c4696a2af09bec70cc08eab"
   end
   license "MIT"
 

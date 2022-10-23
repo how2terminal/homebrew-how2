bb0ef9df18491561ef0cdf89cbc00049a0955126
santinic <hireclaudio@gmail.com>
2022-10-23 19:33:11 +0100

    fix

--- how2.rb
+++ how2.rb
@@ -3,10 +3,10 @@ class How2 < Formula
   homepage "https://how2terminal.com"
   if Hardware::CPU.intel?
     url "https://github.com/santinic/how2/releases/download/v3.0.3/how2-macos-x64.tar.gz"
-    sha256 "ee0a59d7d13ebd56a483387d13443a449b651a8c8edf562c6a2e463714da4900"
+    sha256 "376fac34c586535f0fcd81120e2041595f1d7c4d1b74e4fde64f0a4c85bced80"
   else
     url "https://github.com/santinic/how2/releases/download/v3.0.3/how2-macos-arm64.tar.gz"
-    sha256 "0af24f7bd98a8ffba3b55c94dda84e39083a1bb2a9dc6375a2b58289a4a6295e"
+    sha256 "c90219428e65594c24216dc7e626e8bc7c7178deac24d81fb1475cb7938ac5ea"
   end
   license "MIT"
 

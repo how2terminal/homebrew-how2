84f61ddebd7ee6e884cbd323d637209dd79f8ea3
santinic <hireclaudio@gmail.com>
2022-10-27 19:41:03 +0100

    sha

--- how2.rb
+++ how2.rb
@@ -2,7 +2,7 @@ class How2 < Formula
   desc "AI for your terminal"
   homepage "https://how2terminal.com"
   url "https://github.com/santinic/how2/releases/download/v3.0.3/how2.js.tar.gz"
-  sha256 "bb08cb033f68270598f72853d78131341f4499aec3710d3d9fe0ef69fd52f17b"
+  sha256 "4eb384b93374efae960ec3a235a25c139fe41e0d69036bb609c25c82a9ae98e0"
   license "MIT"
   depends_on "node"
 

4cab7e91409128b6863721ab89a0d80c6a8530e4
santinic <hireclaudio@gmail.com>
2022-10-27 19:38:57 +0100

    js based

--- how2.rb
+++ how2.rb
@@ -1,21 +1,13 @@
 class How2 < Formula
   desc "AI for your terminal"
   homepage "https://how2terminal.com"
-  if Hardware::CPU.intel?
-    url "https://github.com/santinic/how2/releases/download/v3.0.3/how2-macos-x64.tar.gz"
-    sha256 "d4c8b1df1f29e4c631fdd40283870ad4594c0d2a2010f2133aa82d6a8dacde5b"
-  else
-    url "https://github.com/santinic/how2/releases/download/v3.0.3/how2-macos-arm64.tar.gz"
-    sha256 "129d056799c46130d65ef5731a0848aeb8322ac85c5a069fc15d93552e9f2716"
-  end
+  url "https://github.com/santinic/how2/releases/download/v3.0.3/how2.js.tar.gz"
+  sha256 "bb08cb033f68270598f72853d78131341f4499aec3710d3d9fe0ef69fd52f17b"
   license "MIT"
+  depends_on "node"
 
   def install
-    if Hardware::CPU.intel?
-      bin.install "how2-macos-x64" => "how2"
-    else
-      bin.install "how2-macos-arm64" => "how2"
-    end
+    bin.install "how2.js" => "how2"
   end
 
   test do
